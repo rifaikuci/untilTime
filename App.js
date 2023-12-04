6d584be7adbcc83b2fabe2a5564448afef310260
rifaikuci <rifaikuci@gmail.com>
2023-12-04 16:07:40 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text, View
+} from "react-native";
+
+
+
+
+
+
+
+function App() {
+
+  return (
+    <SafeAreaView>
+      <Text>
+        asdas
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
